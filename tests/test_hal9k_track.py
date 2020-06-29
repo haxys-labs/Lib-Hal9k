81355f7503eb904cfbd99fc769395687a8a77ae6
Christopher Steffen <cmsteffen@protonmail.com>
2020-06-29 15:17:46 -0500

    Implemented Track.stop function.

--- tests/test_hal9k_track.py
+++ tests/test_hal9k_track.py
@@ -7,14 +7,18 @@ from hal9k import Track
 
 class Hal9kTrackTest(TestCase):
     """Tests for the hal9k.Track class."""
+
     def setUp(self):
         """Set up for the Track tests."""
         self.progress = mock.MagicMock()
         self.machine = mock.MagicMock()
         self.machine.launch_vm_process.return_value = self.progress
+        self.machine.power_down.return_value = self.progress
         self.vbox = mock.MagicMock()
         self.vbox.find_machine.return_value = self.machine
         self.session = mock.MagicMock()
+        self.console = mock.MagicMock()
+        self.session.console = self.console
 
     @mock.patch("hal9k.track.virtualbox.Session")
     def test_track_init(self, mock_Session):
@@ -41,3 +45,15 @@ class Hal9kTrackTest(TestCase):
                 self.session, "headless", ""
             )
             self.progress.wait_for_completion.assert_called()
+
+    @mock.patch("hal9k.track.virtualbox.Session")
+    def test_track_stop(self, mock_Session):
+        """Test the Track.stop() function."""
+        # Set up test environment.
+        mock_Session.return_value = self.session
+        # Spawn the `Track` class.
+        with Track("demo track", self.vbox) as track:
+            # Stop the track.
+            track.stop()
+            # Check what happened.
+            self.session.console.power_down.assert_called()
