277ed1885c8f9e7735b87363c43e188ea1d6efcf
Christopher Steffen <cmsteffen@protonmail.com>
2020-06-29 15:03:40 -0500

    Refactoring tests before adding second function.

--- tests/test_hal9k_track.py
+++ tests/test_hal9k_track.py
@@ -7,26 +7,37 @@ from hal9k import Track
 
 class Hal9kTrackTest(TestCase):
     """Tests for the hal9k.Track class."""
+    def setUp(self):
+        """Set up for the Track tests."""
+        self.progress = mock.MagicMock()
+        self.machine = mock.MagicMock()
+        self.machine.launch_vm_process.return_value = self.progress
+        self.vbox = mock.MagicMock()
+        self.vbox.find_machine.return_value = self.machine
+        self.session = mock.MagicMock()
+
+    @mock.patch("hal9k.track.virtualbox.Session")
+    def test_track_init(self, mock_Session):
+        """Test Track initialization."""
+        # Set up test environment.
+        mock_Session.return_value = self.session
+        # Spawn the `Track` class.
+        with Track("demo track", self.vbox) as track:
+            # Check that the track was properly instantiated.
+            mock_Session.assert_called()
+            self.vbox.find_machine.assert_called_with("demo track")
 
     @mock.patch("hal9k.track.virtualbox.Session")
     def test_track_play(self, mock_Session):
         """Test the Track.play() function."""
         # Set up test environment.
-        progress = mock.MagicMock()
-        machine = mock.MagicMock()
-        machine.launch_vm_process.return_value = progress
-        vbox = mock.MagicMock()
-        vbox.find_machine.return_value = machine
-        session = mock.MagicMock()
-        mock_Session.return_value = session
+        mock_Session.return_value = self.session
         # Spawn the `Track` class.
-        with Track("demo track", vbox) as track:
+        with Track("demo track", self.vbox) as track:
             # Play the track.
             track.play()
             # Check what happened.
-            mock_Session.assert_called()
-            vbox.find_machine.assert_called_with("demo track")
-            machine.launch_vm_process.assert_called_with(
-                session, "headless", ""
+            self.machine.launch_vm_process.assert_called_with(
+                self.session, "headless", ""
             )
-            progress.wait_for_completion.assert_called()
+            self.progress.wait_for_completion.assert_called()
